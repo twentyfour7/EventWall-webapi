1b340676af33b8f3a6ae033eff0f1dee31f6719c
JJC <jjc@nlplab.cc>
2016-12-04 21:10:41 +0800

    change OrgEvent inheritance

--- representers/organization_events.rb
+++ representers/organization_events.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-require_relative 'event'
+require_relative 'events'
 require_relative 'organization'
 
 # Represents overall organization information for JSON API output
-class OrganizationEventsRepresenter < Roar::Decorator
+class OrganizationEventsRepresenter < EventsRepresenter
   include Roar::JSON
 
   property :organization, extend: OrganizationRepresenter, class: Organization
-  collection :events, extend: EventRepresenter, class: Event
 end
