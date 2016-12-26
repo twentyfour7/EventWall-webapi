4454eeb61602ce79fe68ec7bffee1c994deab9d0
HY.C <rr1515c@gmail.com>
2016-12-27 00:55:42 +0800

    adjusted rule of event_type_filter

--- services/assign_event_type.rb
+++ services/assign_event_type.rb
@@ -4,7 +4,7 @@
 class AssignEventType
   def self.call(org_name, event)
     # preliminarily filtering of event_type
-    return '比賽' if event.title.include?('比賽') || event.summary.include?('比賽')
+    return '比賽' if event.title.include?('比賽' || '競賽' || '大賽' || '盃' || '獎' || '徵文' || '徵件') || event.summary.include?('比賽' || '競賽' || '大賽' || '參賽' || '盃')
     # return '徵才' if event.title.include?('徵才' || '招募' || '誠徵')
     return '社團' if org_name.include?('社')
     '講座'
