0206da3ae21ecf69df6e9ed24528653a63e9cc08
shannywu <shanny@nlplab.cc>
2017-01-08 18:36:17 +0800

    update assign_event_type service

--- services/assign_event_type.rb
+++ services/assign_event_type.rb
@@ -4,10 +4,11 @@
 class AssignEventType
   def self.call(org_name, title, content)
     # preliminarily filtering of event_type
-    return '競賽' if ['比賽', '競賽', '大賽', '盃', '獎', '徵文', '徵件'].any? { |s| title.include? s } || ['比賽', '競賽', '大賽', '盃', '獎', '徵文', '徵件'].any? { |s| content.include? s }
-    # return '徵才' if event.title.include?('徵才' || '招募' || '誠徵')
-    return '社團' if ['聚會', '工作坊', '社群'].any? { |s| title.include? s }
+    return '藝文' if org_name == '清華藝文活動'
+    return '競賽' if ['比賽', '競賽', '大賽', '盃', '徵文', '徵件'].any? { |s| title.include? s } || ['比賽', '競賽', '大賽', '盃', '徵文', '徵件'].any? { |s| content.include? s }
+    return '徵才' if ['徵才', '招募', '誠徵'].any? { |s| title.include? s }
+    return '社團' if ['聚會', '社群'].any? { |s| title.include? s }
     return '社團' if org_name.include?('社')
-    '講座'
+    '學術'
   end
 end
